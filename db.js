b3e6f1b0abfe88deea2d1b093fc955442dd42333
NandeeshHD <nandeesh.hd@gmail.com>
2015-01-08 00:54:05 +0100

    db

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 module.exports = {
 	//'url' : 'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>'
-    //'url' : 'mongodb://videochat:videochat@ds049150.mongolab.com:49150/videochat'
-	'url' : 'mongodb://localhost:27017/videochat'
+    'url' : 'mongodb://videochat:videochat@ds049150.mongolab.com:49150/videochat'
+	//'url' : 'mongodb://localhost:27017/videochat'
 }
